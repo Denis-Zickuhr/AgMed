0cdedec0d7f87ee4d73d4095a90f576d9f0d8a3c
TekkenRX <GameEend00@gmail.com>
2021-06-25 11:29:40 -0300

    foi?

---
+++ src/agmed/Agenda.java
@@ -0,0 +1,21 @@
+
+package agmed;
+
+
+public class Agenda {
+    private String Medico;
+    /**
+    public Agenda(String medico){
+        this.Medico = Medico;
+    }
+
+    public String getMedico() {
+        return Medico;
+    }
+    
+    ArrayList<String> Consulta = new ArrayList<>();
+    
+    public void addConsulta
+    
+    **/
+}
